e6ed4f025a2db72267e4c224b2c88ffbae0f55a9
felipefn15 <felipefrancanogueira@gmail.com>
2022-11-11 17:51:24 +0100

    WIP: Filter just favorites

--- src/components/home/index.tsx
+++ src/components/home/index.tsx
@@ -1,36 +1,15 @@
-import React, { useState } from "react";
-import { HomeFilled, HomeOutlined, StarFilled, StarOutlined } from "@ant-design/icons";
+import React from "react";
+// import InfiniteScroll from 'react-infinite-scroll-component';
 import { HomeProps, Pokemon } from "../../types";
 import PokemonCard from "../pokemonCard";
-import ButtonGroup from 'react-bootstrap/ButtonGroup';
-import ToggleButton from 'react-bootstrap/ToggleButton';
 import './index.css';
 
 function Home(props: HomeProps) {
-  // const [onlyFavorite, setOnlyFavorite] = useState('1')
-  // const radios = [
-  //   { icon: <HomeFilled />, value: '1' },
-  //   { icon: <StarFilled />, value: '2' },
-  // ];
+
 
   return (
     <div className="homeWrapper">
-      {/* <ButtonGroup>
-        {radios.map((radio, idx) => (
-          <ToggleButton
-            key={idx}
-            id={`radio-${idx}`}
-            type="radio"
-            variant={idx % 2 ? 'outline-success' : 'outline-danger'}
-            name="radio"
-            value={radio.value}
-            checked={onlyFavorite === radio.value}
-            onChange={(e) => setOnlyFavorite(e.currentTarget.value)}
-          >
-            {radio.icon}
-          </ToggleButton>
-        ))}
-      </ButtonGroup> */}
+
       {
         props.pokemons.map((pokemon: Pokemon) => {
           return PokemonCard(pokemon)
